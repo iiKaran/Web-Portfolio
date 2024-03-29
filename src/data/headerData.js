197b2b6172d637263ed394294f95e3785898e5f4
karandevelops <karankumar560k@gmail.com>
2024-03-29 09:35:31 +0530

    updated a bit

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,5 +5,5 @@ export const headerData = {
     desciption: "Hello! I am Karan Sehgal, a dedicated Full Stack Web & App Developer. I bring a solid foundation  Problem Solving  and more. I have a knack for algorithmic problem-solving, having tackled over 500+ coding questions.",
     image: 'https://prompti.ai/wp-content/uploads/2023/07/pcboi2.png',
     // imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
-    resumePdf: 'https://drive.google.com/file/d/1GPbzpR6YXVtATEZLXhj-eoG9Hp3Yrnen/view?usp=sharing'
+    resumePdf: 'https://drive.google.com/file/d/167O0waG9tjU4YPPwKH0jOrhEJVti3lkk/view?usp=sharing'
 }
